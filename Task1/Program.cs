0c4cbb55d8953e532c78bb7fcddbb5ba7cdd78fc
DESKTOP-M3Q4HRF\PCHome <jaroschew@gmail.com>
2025-09-17 10:08:43 +0300

    Add Lab1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
